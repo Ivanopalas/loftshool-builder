1b4aa221714ec72b0e26e7617c6ac814a8330862
Ivanopalas <ivan.aksyutin@gmail.com>
2016-06-21 18:01:31 +0300

    html done all pages

--- gulp/paths/template.js
+++ gulp/paths/template.js
@@ -4,5 +4,6 @@ module.exports = [
   './source/template/index.jade',
   './source/template/about.jade',
   './source/template/works.jade',
-  './source/template/blog.jade'
+  './source/template/blog.jade',
+  './source/template/login.jade'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,18 @@
-(function() {
-  'use strict';
-
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+function setEqualHeight(columns)
+{
+    var tallestcolumn = 0;
+    columns.each(
+        function()
+        {
+            currentHeight = $(this).height();
+            if(currentHeight > tallestcolumn)
+            {
+                tallestcolumn = currentHeight;
+            }
+        }
+    );
+    columns.children().height(tallestcolumn);
+}
+$(document).ready(function() {
+    setEqualHeight($(".js-equalheight > div"));
+});
